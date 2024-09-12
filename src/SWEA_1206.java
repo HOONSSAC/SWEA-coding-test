697badfd9ee91b94bcf6c7aaf3795377a5656979
b1uesoda <b1uesoda@naver.com>
2024-09-12 15:20:17 +0900

    SWEA_1206

---
+++ src/SWEA_1206.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.FileInputStream;
+
+public class SWEA_1206 {
+	public static void main(String args[]) throws Exception
+	{
+		/*
+		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
+		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
+		 */
+		System.setIn(new FileInputStream("res/input.txt"));
+
+		/*
+		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
+		 */
+		Scanner sc = new Scanner(System.in);
+
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+		for(int test_case = 1; test_case <= 10; test_case++)
+		{
+			int answer = 0;
+			int count = sc.nextInt(); // 건물 개수
+			
+			// 건물의 개수가 4면 0반환
+			if (count == 4) {
+				System.out.println("#" + test_case + " " + answer);
+			}
+			else {
+				int[] buildings = new int[count];
+				for (int i = 0; i < count; i++) {
+					buildings[i] = sc.nextInt(); // 건물 높이 입력 
+				}
+				
+				
+				
+				for (int i = 2; i < count - 2; i++) {
+					int now = buildings[i];
+					int a = buildings[i-2];
+					int b = buildings[i-1];
+					int c = buildings[i+1];
+					int d = buildings[i+2];
+					
+					if (a < now && b < now && c < now && d < now) {
+						answer += (now - Math.max(Math.max(a, b), Math.max(c, d)));
+					
+					}
+					
+
+				}
+				System.out.println("#" + test_case + " " + answer);
+			}
+		}
+		sc.close();
+	}
+}
